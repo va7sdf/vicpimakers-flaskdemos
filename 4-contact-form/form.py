a7c075937a638a40cb39512126b272bbf90079d8
Gordon M. Celesta <gordo@sdf.lonestar.org>
2021-11-26 20:10:19 -0800

    Make text field required

--- 4-contact-form/form.py
+++ 4-contact-form/form.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired, Email
 class ContactForm(FlaskForm):
 	name = StringField(label='Name', validators=[DataRequired()])
 	email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
-	message= TextAreaField(label='Message')
+	message= TextAreaField(label='Message', validators=[DataRequired()])
 	submit = SubmitField(label="Send")
