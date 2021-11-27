b4c974093ef2665a6879c24b8cee2364b34d240c
Gordon M. Celesta <gordo@sdf.lonestar.org>
2021-11-26 19:56:28 -0800

    Add and update comments

--- 4-contact-form/app.py
+++ 4-contact-form/app.py
@@ -7,9 +7,13 @@
 # Gordon M. Celesta
 # gordo@sdf.lonestar.org
 #
-# FURTHER READING
+# INSPIRED BY
 #
 # https://www.geeksforgeeks.org/create-contact-us-using-wtforms-in-flask/
+#
+# FURTHER READING
+# https://flask.palletsprojects.com/en/2.0.x/config/#configuration-best-practices
+# 
 ########################################################################
 
 from flask import Flask, render_template
@@ -17,15 +21,22 @@ from form import ContactForm
 
 app = Flask(__name__)
 
-# import secrets
-# secrets.token_hex(16)
+# Execute the following in a Python interpreter to generate a random
+# secret key:
+# >>> import secrets
+# >>> secrets.token_hex(16)
+
+# DO NOT HARD CODE YOUR SECRET KEY,
+# for demonstration purposes only
 app.secret_key = '2283fe9058fe7c38c1ca61c55c5540ed'
 
 @app.route("/", methods=["GET", "POST"])
 def contact_form():
 
+    # instantiate form
     form = ContactForm()
 
+    # validate form input on POST
     if form.validate_on_submit():
         name = form.name.data
         email = form.email.data
